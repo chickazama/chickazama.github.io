14e9b175c43d0665bc20554c71ce4afd056e1ceb
Matthew Hope <chickazama@gmail.com>
2024-06-17 10:33:01 +0100

    Update fetch

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 import * as components from "./components.js";
-// import * as articles from "./articles/example.json";
 
 window.addEventListener("DOMContentLoaded", initHomeViewAsync);
 
 async function initHomeViewAsync() {
-    // const res = await fetch("./js/articles/example.json");
-    // const body = await res.json();
-    // console.log(body);
+    const res = await fetch("./data/articles.json");
+    const body = await res.json();
+    console.log(body);
     const nav = components.Navbar();
     document.body.appendChild(nav);
 }
