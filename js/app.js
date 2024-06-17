925cbe2aa7781f50c532162c1aa24e1e6ec25864
Matthew Hope <chickazama@gmail.com>
2024-06-17 10:25:51 +0100

    Test importing JSON

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 import * as components from "./components.js";
-// import * as articles from "./articles/example.json";
+import * as articles from "./articles/example.json";
 
 window.addEventListener("DOMContentLoaded", initHomeViewAsync);
 
 async function initHomeViewAsync() {
-    // console.log(articles);
+    console.log(articles);
     const nav = components.Navbar();
     document.body.appendChild(nav);
 }
