daf65936573a8e6309c8114c44c6e8ad88c53fb8
Matthew Hope <chickazama@gmail.com>
2024-06-17 10:29:18 +0100

    Remove redundant Navbar links

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 import * as components from "./components.js";
-import * as articles from "./articles/example.json";
+// import * as articles from "./articles/example.json";
 
 window.addEventListener("DOMContentLoaded", initHomeViewAsync);
 
 async function initHomeViewAsync() {
-    console.log(articles);
+    const res = await fetch("./js/articles/example.json");
+    const body = await res.json();
+    console.log(body);
     const nav = components.Navbar();
     document.body.appendChild(nav);
 }
