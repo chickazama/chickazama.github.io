e97276bddfa8608ad45b2619d91dbbdd76e86bd1
Matthew Hope <chickazama@gmail.com>
2024-06-17 10:56:01 +0100

    Include site logo in Navbar

--- js/components.js
+++ js/components.js
@@ -10,7 +10,9 @@ export function Navbar() {
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">
+            <img src="/img/site-logo.png" width="32" height="32" />
+          </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
