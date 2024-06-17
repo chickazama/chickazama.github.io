b9aacf429f146249dc5d9838df3bd8bf3aca727e
Matthew Hope <chickazama@gmail.com>
2024-06-17 10:30:13 +0100

    Comment out JSON load

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ import * as components from "./components.js";
 window.addEventListener("DOMContentLoaded", initHomeViewAsync);
 
 async function initHomeViewAsync() {
-    const res = await fetch("./js/articles/example.json");
-    const body = await res.json();
-    console.log(body);
+    // const res = await fetch("./js/articles/example.json");
+    // const body = await res.json();
+    // console.log(body);
     const nav = components.Navbar();
     document.body.appendChild(nav);
 }
