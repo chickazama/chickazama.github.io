fdb3f50a288b4ced06f30db10552354c242b033c
Matthew Hope <chickazama@gmail.com>
2024-06-17 10:49:35 +0100

    Define articles global variable

--- js/app.js
+++ js/app.js
@@ -2,7 +2,13 @@ import * as components from "./components.js";
 
 window.addEventListener("DOMContentLoaded", initHomeViewAsync);
 
+let articles;
+
 async function initHomeViewAsync() {
+    const res = await fetch("/js/data/articles.json");
+    articles = await res.json();
+    console.log("Articles:");
+    console.log(articles);
     const header = document.createElement("header");
     const nav = components.Navbar();
     header.appendChild(nav);
